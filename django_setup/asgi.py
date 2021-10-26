44370c72922beabe15cf83b70200a509718d0ceb
JC_onLine <jc.gillot@gmail.com>
2021-10-26 12:36:07 +0200

    refact(django): Rename config folder to 'django_setup'

--- django_setup/asgi.py
+++ django_setup/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for django_webpack_template project.
+ASGI config for django_setup project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_webpack_template.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_setup.settings')
 
 application = get_asgi_application()
--- django_setup/urls.py
+++ django_setup/urls.py
@@ -1,4 +1,4 @@
-"""django_webpack_template URL Configuration
+"""django_setup URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
