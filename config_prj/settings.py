1c1098c17a4e59c9fd2da5ffefba8030fcfecbb3
JC_onLine <jc.gillot@gmail.com>
2021-10-27 13:05:37 +0200

    build(django): static & css.

--- config_prj/settings.py
+++ config_prj/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 Django stack: Django + Webpack + Sass + VueJS
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -122,6 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
