c3ab21520d26cfe25ebf49603251a0807b3b030b
JC_onLine <jc.gillot@gmail.com>
2021-10-27 09:33:50 +0200

    doc(django): Settings stack description.

--- config_prj/settings.py
+++ config_prj/settings.py
@@ -8,6 +8,8 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
+
+Django stack: Django + Webpack + Sass + VueJS
 """
 
 from pathlib import Path
